8b7b242da3248ee317e3d79ce6c84f50eac104f1
chenyu1229 <chenyuli1229@gmail.com>
2024-06-01 17:38:33 -0400

    change functions

--- src/model_prediction.py
+++ src/model_prediction.py
@@ -8,28 +8,56 @@ import keras
 from keras import layers
 from matplotlib import pyplot as plt
 import training_var
+import sys
 
-df_test = pd.read_csv('../data/test.csv',parse_dates=True, index_col="timestamp",header=0)
-model = keras.models.load_model('../res/model.keras')
 
-TIME_STEPS = int(24*7)
-
+def read_dataset(file):
+    df = pd.read_csv(file, parse_dates=True, index_col="timestamp",header=0)
+    return df
 
 # Generated training sequences for use in the model.
-def create_sequences(values, time_steps=TIME_STEPS):
+def create_sequences(values, time_steps):
     output = []
     for i in range(len(values) - time_steps + 1):
         output.append(values[i : (i + time_steps)])
     return np.stack(output)
 
-df_test_value = (df_test - training_var.training_mean) / training_var.training_std
+def pre_processing(df,TIME_STEPS):
+    df = df.dropna()
+    df_test_value = (df - training_var.training_mean) / training_var.training_std
+    # Create sequences from test values.
+    x_test = create_sequences(df_test_value.values,TIME_STEPS)
+    print("Test input shape: ", x_test.shape)
+    return x_test
+
+def univariate_anomalous_data(anomalies):
+    anomalous_data_indices_raw = np.where(anomalies)[0]
+    anomalous_data_indices_raw = [x+TIME_STEPS/2 for x in anomalous_data_indices_raw]
+    anomalous_data_indices = []
+    for data_idx in anomalous_data_indices_raw:
+        if set(range(int(data_idx)-int(TIME_STEPS/4),int(data_idx)+int(TIME_STEPS/4))).issubset(set(anomalous_data_indices_raw)):
+            anomalous_data_indices.append(int(data_idx))
+    print("Number of anomaly samples: ", len(anomalous_data_indices))
+    print("Indices of anomaly samples: ", anomalous_data_indices)
+    return anomalous_data_indices
+
+def generate_res(df_test):
+    df_res = df_test.drop('value', axis=1).reset_index()
+    df_res['res'] = False
+    df_res.loc[anomalous_data_indices,'res'] = True
+    df_res.to_csv("../res/res.csv",index=False)
+
+TIME_STEPS = training_var.time_steps
+df_test = read_dataset('../data/test.csv')
+# df_test = pd.read_csv('../data/test.csv',parse_dates=True, index_col="timestamp",header=0)
+x_test = pre_processing(df_test,TIME_STEPS)
+model = keras.models.load_model('../res/model.keras')
+
+
 # fig, ax = plt.subplots()
 # df_test_value.plot(legend=False, ax=ax)
 # plt.show()
 
-# Create sequences from test values.
-x_test = create_sequences(df_test_value.values)
-print("Test input shape: ", x_test.shape)
 
 # Get test MAE loss.
 x_test_pred = model.predict(x_test)
@@ -45,20 +73,9 @@ test_mae_loss = test_mae_loss.reshape((-1))
 anomalies = test_mae_loss > training_var.threshold
 
 
-anomalous_data_indices_raw = np.where(anomalies)[0]
-anomalous_data_indices_raw = [x+TIME_STEPS/2 for x in anomalous_data_indices_raw]
-anomalous_data_indices = []
-for data_idx in anomalous_data_indices_raw:
-    if set(range(int(data_idx)-int(TIME_STEPS/4),int(data_idx)+int(TIME_STEPS/4))).issubset(set(anomalous_data_indices_raw)):
-        anomalous_data_indices.append(int(data_idx))
-        
+anomalous_data_indices = univariate_anomalous_data(anomalies)
 
-print("Number of anomaly samples: ", len(anomalous_data_indices))
-print("Indices of anomaly samples: ", anomalous_data_indices)
 
-df_res = df_test.drop('value', axis=1).reset_index()
-df_res['res'] = False
-df_res.loc[anomalous_data_indices,'res'] = True
-df_res.to_csv("../res/res.csv",index=False)
+generate_res(df_test)
 
 
--- src/model_training.py
+++ src/model_training.py
@@ -7,19 +7,13 @@ import numpy as np
 import keras
 from keras import layers
 from matplotlib import pyplot as plt
+import sys
 
 
-df = pd.read_csv('../data/train.csv', parse_dates=True, index_col="timestamp",header=0) 
 
-
-training_mean = df.mean()
-training_std = df.std()
-df_training_value = (df - training_mean) / training_std
-print("Number of training samples:", len(df_training_value))
-
-
-TIME_STEPS = int(24*7)
-
+def read_dataset(file):
+    df = pd.read_csv(file, parse_dates=True, index_col="timestamp",header=0)
+    return df
 
 # Generated training sequences for use in the model.
 def create_sequences(values, time_steps=TIME_STEPS):
@@ -28,31 +22,76 @@ def create_sequences(values, time_steps=TIME_STEPS):
         output.append(values[i : (i + time_steps)])
     return np.stack(output)
 
+def pre_processing(df):
+    df = df.dropna()
+    training_mean = df.mean()
+    training_std = df.std()
+    df_training_value = (df - training_mean) / training_std
+    print("Number of training samples:", len(df_training_value))
+    x_train = create_sequences(df_training_value)
+    print("Training input shape: ", x_train.shape)
+    return x_train, training_mean, training_std
 
-x_train = create_sequences(df_training_value)
-print("Training input shape: ", x_train.shape)
 
-n_steps = x_train.shape[1]
-n_features = x_train.shape[2]
-model = keras.Sequential(
-    [
-        layers.Input(shape=(n_steps, n_features)),
-        layers.Conv1D(filters=32, kernel_size=15, padding='same', data_format='channels_last',
-            dilation_rate=1, activation="linear"),
-        layers.LSTM(
-            units=25, activation="tanh", name="lstm_1", return_sequences=False
-        ),
-        layers.RepeatVector(n_steps),
-        layers.LSTM(
-            units=25, activation="tanh", name="lstm_2", return_sequences=True
-        ),
-        layers.Conv1D(filters=32, kernel_size=15, padding='same', data_format='channels_last',
-            dilation_rate=1, activation="linear"),
-        layers.TimeDistributed(layers.Dense(1, activation='linear'))
-    ]
-)
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
-model.summary()
+
+
+
+
+# x_train = create_sequences(df_training_value)
+# print("Training input shape: ", x_train.shape)
+def build_model(x_train):
+    n_steps = x_train.shape[1]
+    n_features = x_train.shape[2]
+    model = keras.Sequential(
+        [
+            layers.Input(shape=(n_steps, n_features)),
+            layers.Conv1D(filters=32, kernel_size=15, padding='same', data_format='channels_last',
+                dilation_rate=1, activation="linear"),
+            layers.LSTM(
+                units=25, activation="tanh", name="lstm_1", return_sequences=False
+            ),
+            layers.RepeatVector(n_steps),
+            layers.LSTM(
+                units=25, activation="tanh", name="lstm_2", return_sequences=True
+            ),
+            layers.Conv1D(filters=32, kernel_size=15, padding='same', data_format='channels_last',
+                dilation_rate=1, activation="linear"),
+            layers.TimeDistributed(layers.Dense(1, activation='linear'))
+        ]
+    )
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
+    model.summary()
+    return model
+
+
+
+def save_res(train_mae_loss,training_mean,training_std):
+    plt.hist(train_mae_loss, bins=50)
+    plt.xlabel("Training MAE")
+    plt.ylabel("No of samples")
+    plt.savefig("../res/training_MAE_loss.jpg")
+
+    # Get reconstruction loss threshold.
+    threshold = np.max(train_mae_loss)
+    mesg = "threshold="+str(threshold)+"\n"+"training_mean="+str(float(training_mean.iloc[0]))+"\n"+"training_std="+str(float(training_std.iloc[0]))+"\n"+"time_steps="+str(int(TIME_STEPS))
+    print("Reconstruction error threshold: ", threshold)
+
+    model.save('../res/model.keras') 
+    with open("training_var.py","w") as f:
+        f.write(mesg)
+    print("Model Saved")
+
+
+
+
+
+TIME_STEPS = int(24*7)
+if len(sys.argv) > 1:
+    TIME_STEPS = int(sys.argv[1])
+print (TIME_STEPS)
+df = read_dataset('../data/train.csv')
+x_train, training_mean, training_std = pre_processing(df)
+model  = build_model(x_train)
 
 history = model.fit(
     x_train,
@@ -68,25 +107,7 @@ history = model.fit(
 print("Calculating Training MAE Loss")
 x_train_pred = model.predict(x_train)
 train_mae_loss = np.mean(np.abs(x_train_pred - x_train), axis=1)
-
-plt.hist(train_mae_loss, bins=50)
-plt.xlabel("Training MAE")
-plt.ylabel("No of samples")
-plt.savefig("../res/training_MAE_loss.jpg")
-print("Training MAE Loss Image Saved")
-
-# Get reconstruction loss threshold.
-threshold = np.max(train_mae_loss)
-mesg = "threshold="+str(threshold)+"\n"+"training_mean="+str(float(training_mean.iloc[0]))+"\n"+"training_std="+str(float(training_std.iloc[0]))
-print("Reconstruction error threshold: ", threshold)
-
-model.save('../res/model.keras') 
-print("Model Saved")
-with open("training_var.py","w") as f:
-    f.write(mesg)
-print("Loss Threshold Saved")
-
-
+save_res(train_mae_loss,training_mean,training_std)
 
 
 
